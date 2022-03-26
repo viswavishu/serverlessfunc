7b1ef26039c803d6fde58230d66daa12d12e52a4
Viswanadh <viswavishu0110@gmail.com>
2022-03-26 20:16:10 +0530

    adds airtable file for testing as external api

---
+++ functions/airtable.js
@@ -0,0 +1,35 @@
+//use this when using env variables
+// require('dotenv').config()
+const Airtable = require('airtable-node');
+
+const airtable = new Airtable({ apiKey: 'keyR3sFraWsqJZuo6' })
+    .base('appLJw8rcQWLRrqjC')
+    .table('products')
+
+exports.handler = async (event, context, cb) => {
+    try {
+        const { records } = await airtable.list();
+        // console.log(records)
+        const products = records.map((product) => {
+            const { id } = product
+            const { name, image, price, description } = product.fields
+            const url = image[0].url
+            return { id, name, url, price, description }
+        }).join('')
+        return {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+            statusCode: 200,
+            // body: 'Airtable Example',
+            body: JSON.stringify(records),//prints the api to page
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Internal server error',
+        }
+    }
+
+}
+
