a8597e9ffdfd0126efe781d075e3c6717587bdae
Viswanadh <viswavishu0110@gmail.com>
2022-03-26 19:19:07 +0530

    adds corse error fix lets test.

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -4,7 +4,7 @@ const result = document.querySelector('.result')
 
 const fetchData = async() => {
  try{
-     const {data} = await axios.get('/api/2-basic-api');
+     const {data} = await axios.get('https://myserverless.netlify.app/api/2-basic-api');
      const products = data.map((product) => {
       const{image:{url},name,price} = product
       return `<article class="product">
--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,7 +1,10 @@
 const items = require('../assets/data')
 
-exports.handler = async (event, context, cb)=>{
-    return{
+exports.handler = async (event, context, cb) => {
+    return {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
         statusCode: 200,
         // body: 'our 2 basic api',
         body: JSON.stringify(items),
