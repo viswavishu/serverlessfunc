0fe2f48839acfe0ecb364032da5745c28925181f
Viswanadh <viswavishu0110@gmail.com>
2022-03-27 18:27:09 +0530

    adds single product api

---
+++ functions/products.js
@@ -0,0 +1,36 @@
+const Airtable = require('airtable-node');
+
+const airtable = new Airtable({ apiKey: 'keyR3sFraWsqJZuo6' })
+    .base('appLJw8rcQWLRrqjC')
+    .table('items')
+
+exports.handler = async(event, context, cb)=>{
+    const {id} = event.queryStringParameters
+    if(id){
+        try{
+            const product = await airtable.retrieve(id)
+            if(product.error){
+                return{
+                statusCode: 404,
+                body: `No product with id: ${id}`,
+                }
+            }
+            return{
+                statusCode: 200,
+                body:JSON.stringify(product),
+            }
+        }
+        catch(error){
+            return{
+                statusCode: 404,
+                body: `No product with id: ${id}`,
+                }
+
+        }
+       
+    }
+    return{
+        statusCode: 400,
+        body:'please provide product id'
+    }
+}
